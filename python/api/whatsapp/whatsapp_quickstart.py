2e8209ae29246302ccbf1190d588812ee1ad21f3
Airlangga <airlanggakrishna@gmail.com>
2025-09-11 22:23:20 +0700

    add whatsapp dir

---
+++ python/api/whatsapp/whatsapp_quickstart.py
@@ -0,0 +1,41 @@
+from os import getenv
+from urllib import response
+from dotenv import load_dotenv
+import requests
+load_dotenv()
+
+ACCESS_TOKEN = getenv("ACCESS_TOKEN")
+RECIPIENT_WA_NO = getenv("RECIPIENT_WA_NO")
+VERSION = getenv("VERSION")
+PHONE_NUMBER_ID = getenv("PHONE_NUMBER_ID")
+
+if not ACCESS_TOKEN or not RECIPIENT_WA_NO or not VERSION or not PHONE_NUMBER_ID:
+    raise ValueError("One or more environment variables are not set.")
+
+def send_whatsapp_message():
+    url = f"https://graph.facebook.com/{VERSION}/{PHONE_NUMBER_ID}/messages"
+
+    headers = {
+        "Authorization" : "Bearer " + ACCESS_TOKEN,
+        "Content-Type" : "application/json"
+    }
+
+    data = {
+        "messaging_product": "whatsapp",
+        "to": RECIPIENT_WA_NO,
+        "type": "template",
+        "template": {
+            "name": "hello_world",
+            "language": {
+                "code": "en_US"
+            }
+        }
+    }
+
+    response = requests.post(url, headers=headers, json=data)
+
+    return response
+
+response = send_whatsapp_message()
+print(response.status_code)
+print(response.json())
